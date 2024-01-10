96595782286dc2101999cf5070f16251de0e234e
OlegKhiretdinov <oleg@oleg.win>
2024-01-10 19:14:52 +0300

    feat: добавлен функционал создания пользователей

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -3,4 +3,5 @@ from task_manager.users import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='users'),
+    path('create/', views.CreateUserView.as_view(), name='create_user'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
 
+from task_manager.users.forms import CreateUserForm
 
+
+# Список пользователей
 class IndexView(View):
-
     def get(self, request):
         users_list = User.objects.all()
+        return render(request, 'users/index.html', {'users_list': users_list})
 
-        return render(request, 'users/index.html', context={'users_list': users_list})
+
+# Создание пользователя
+class CreateUserView(View):
+    def get(self, request, *args, **kwargs):
+        form = CreateUserForm()
+        return render(request, 'users/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+
+        # return redirect('create_user')
+        return render(request, 'users/create.html', {'form': form})
