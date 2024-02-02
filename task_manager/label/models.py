ac43aac56dfe45bba8bfdd4da0cb95cf9da60107
OlegKhiretdinov <oleg@oleg.win>
2024-02-02 15:31:26 +0300

    fix: исправленна локализация

--- task_manager/label/models.py
+++ task_manager/label/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class LabelModel(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name=_('name'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
+
 from task_manager.task_status.models import TaskStatusModel
 from task_manager.label.models import LabelModel
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+")
-    executor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+", blank=True, null=True)
-    status = models.ForeignKey(TaskStatusModel, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255, verbose_name=_('name'))
+    description = models.TextField(verbose_name=_('description'))
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+", verbose_name=_('author'))
+    executor = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+", blank=True, null=True, verbose_name=_('executor'))
+    status = models.ForeignKey(TaskStatusModel, on_delete=models.PROTECT, verbose_name=_('status'))
     created_at = models.DateTimeField(auto_now_add=True)
     labels = models.ManyToManyField(
         LabelModel,
         through='TaskLabelRelation',
         through_fields=('task', 'label'),
         blank=True,
+        verbose_name=_('Labels'),
     )
 
     def __str__(self):
