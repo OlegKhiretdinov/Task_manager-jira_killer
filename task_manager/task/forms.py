830310ba6a9572ee8967b5d89370179124ee5946
OlegKhiretdinov <oleg@oleg.win>
2024-02-03 13:14:32 +0300

    fix: form

--- task_manager/task/forms.py
+++ task_manager/task/forms.py
@@ -5,4 +5,4 @@ from .models import Task
 class TaskCreateForm(ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'executor', 'status', 'labels']
+        fields = ['name', 'description', 'status', 'executor', 'labels']
